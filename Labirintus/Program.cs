b4b1f213da99561a25e1903760b6ba2245327a79
KomlosiBalazs <70943526+KomlosiBalazs@users.noreply.github.com>
2021-02-09 11:29:49 +0100

    Add files via upload
    2012,02,09

--- Labirintus/Program.cs
+++ Labirintus/Program.cs
@@ -18,12 +18,15 @@ namespace Labirintus
             while (gameisrunning)
             {
                 char command = Console.ReadKey().KeyChar;
-                switch (command) {
+                Console.Clear();
+                switch (command)
+                {
+
                     case 'w': map1.up(); break;
                     case 's': map1.down(); break;
                     case 'a': map1.left(); break;
                     case 'd': map1.right(); break;
-                    case 'f': Console.WriteLine(map1.whereamI()[0]+'|'+map1.whereamI()[1]); ; break;
+                    case 'f': Console.WriteLine(map1.whereamI()[0] + '|' + map1.whereamI()[1]); ; break;
                     default:
                         Console.WriteLine("nincs ilyen utasítás");
                         break;
@@ -31,6 +34,7 @@ namespace Labirintus
 
                 Console.WriteLine(map1.getName());
                 map1.showMap();
+                map1.showAndclearMessage();
 
             }
             Console.ReadKey();
@@ -40,45 +44,59 @@ namespace Labirintus
         {
             private char[,] map;
             private String mapname;
+            private string message;
             public Maphandler(String name)
             {
                 this.mapname = name;
+                this.message = ""; 
                 this.map = new char[,]
                     {
-                    { '#', '@','#','#','#','#'},
+                    { '#', '.','#','#','#','#'},
                     { '#', '.','#','.','.','#'},
                     { '#', '.','.','#','.','x'},
                     { '#', '#','.','#','.','#'},
-                    { '.', '.','.','.','.','#'},
-                    { '#', '.','.','#','.','#'},
-                    { '#', '.','#','#','#','#'},
+                    { '.', '.','@','.','.','#'},
+                    { '#', '#','#','#','.','#'},
 
                     };
             }
 
-                public String getName()
+            public String getName()
+            {
+                return mapname;
+
+            }
+
+            public char[,] getMap()
+            {
+                return map;
+            }
+
+            public void showMap()
+            {
+                int meret = this.map.GetLength(0);
+                for (int row = 0; row < meret; row++)
                 {
-                    return mapname;
+                    for (int col = 0; col < meret; col++)
+                    {
 
-                }
+                        Console.Write(this.map[row, col] + " ");
 
-                public char[,] getMap() {
-                    return map;
-                }
-
-                public void showMap() {
-                    int meret = this.map.GetLength(0);
-                    for (int row = 0; row < meret; row++) {
-                        for (int col = 0; col < meret; col++) {
-                            Console.Write(this.map[row, col]+" ");
-
-                        }
+                    }
                     Console.WriteLine();
+
                 }
 
 
             }
-            public int[] whereamI() {
+            public void showAndclearMessage() {
+                
+                Console.WriteLine(this.message);
+                this.message = "";
+            
+            }
+            public int[] whereamI()
+            {
                 int[] pos = { 0, 0 };
                 int meret = this.map.GetLength(0);
                 for (int row = 0; row < meret; row++)
@@ -89,7 +107,7 @@ namespace Labirintus
                         {
                             pos[0] = row;
                             pos[1] = col;
-                        
+
                         }
 
                     }
@@ -98,12 +116,133 @@ namespace Labirintus
 
                 return pos;
             }
-            public void up() { }
-            public void down() { }
-            public void left() { }
-            public void right() { }
+            public void up()
+            {
+                int[] pos = whereamI();
 
+                int x = pos[0];
+                int y = pos[1];
+
+                try
+                {
+                    if (map[x - 1, y] == '.')
+                    {
+                        map[x - 1, y] = '@';
+                        map[x, y] = '.';
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("falba ütköztél");
+                    }
+
+
+
+                }
+
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("elérted a pálya szélét");
+
+                }
             }
+            public void down()
+            {
+                int[] pos = whereamI();
+
+                int x = pos[0];
+                int y = pos[1];
+
+                try
+                {
+                    if (map[x + 1, y] == '.')
+                    {
+                        map[x + 1, y] = '@';
+                        map[x, y] = '.';
+
+                    }
+                    else
+                    {
+
+                        this.message+="falba ütköztél";
+                    }
+
+
+
+                }
+
+                catch (IndexOutOfRangeException e)
+                {
+                    this.message+="elérted a pálya szélét";
+
+                }
+            }
+            public void left()
+            {
+                int[] pos = whereamI();
+
+                int x = pos[0];
+                int y = pos[1];
+
+                try
+                {
+                    if (map[x, y - 1] == '.')
+                    {
+                        map[x, y - 1] = '@';
+                        map[x, y] = '.';
+
+                    }
+                    else
+                    {
+
+                        this.message += "falba ütköztél";
+                    }
+
+
+
+                }
+
+                catch (IndexOutOfRangeException e)
+                {
+                    this.message += "elérted a pálya szélét";
+
+                }
+            }
+
+            public void right()
+            {
+                int[] pos = whereamI();
+
+                int x = pos[0];
+                int y = pos[1];
+
+                try
+                {
+                    if (map[x, y + 1] == '.')
+                    {
+                        map[x, y + 1] = '@';
+                        map[x, y] = '.';
+
+                    }
+                    else
+                    {
+
+                        this.message += "falba ütköztél";
+                    }
+
+
+
+                }
+
+                catch (IndexOutOfRangeException e)
+                {
+                    this.message += "elérted a pálya szélét";
+
+                }
+            }
+
         }
     }
+}
 
